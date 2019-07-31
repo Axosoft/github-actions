130819ffc3b5da418e8519407c455d76e720f61d
Dan Suceava <dans@axosoft.com>
2019-07-31 14:27:54 -0700

    more debug

--- glo/move-cards/lib/index.js
+++ glo/move-cards/lib/index.js
@@ -25,8 +25,9 @@ function run() {
         const authToken = core.getInput('authToken');
         const boardID = core.getInput('boardID');
         const cardID = core.getInput('cardID');
-        core.debug(`ids: ${boardID}  ${cardID}`);
         core.debug(`auth: ${authToken}`);
+        core.debug(`token len: ${authToken.length}`);
+        core.debug(`matches: ${authToken === 'pd899c05b2538cbc64fb7c2c68cc279fe9239b3e2'}`);
         try {
             yield glo_sdk_1.default(authToken).boards.cards.comments.create(boardID, cardID, {
                 text: 'hello from GH actions'
--- glo/move-cards/src/index.ts
+++ glo/move-cards/src/index.ts
@@ -6,8 +6,9 @@ async function run() {
   const boardID = core.getInput('boardID');
   const cardID = core.getInput('cardID');
 
-  core.debug(`ids: ${boardID}  ${cardID}`);
   core.debug(`auth: ${authToken}`);
+  core.debug(`token len: ${authToken.length}`);
+  core.debug(`matches: ${authToken === 'pd899c05b2538cbc64fb7c2c68cc279fe9239b3e2'}`);
 
   try {
     await GloSDK(authToken).boards.cards.comments.create(boardID, cardID, {
