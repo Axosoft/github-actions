98728535f023419ffa0a133a86066ab171ab829c
Dan Suceava <dans@axosoft.com>
2019-08-05 09:30:32 -0700

    remove the debugs

--- glo/move-cards/src/index.ts
+++ glo/move-cards/src/index.ts
@@ -6,10 +6,6 @@ async function run() {
   const boardID = core.getInput('boardID');
   const cardID = core.getInput('cardID');
 
-  core.debug(`auth: ${authToken}`);
-  core.debug(`token len: ${authToken.length}`);
-  core.debug(`matches: ${authToken === 'pd899c05b2538cbc64fb7c2c68cc279fe9239b3e2'}`);
-
   try {
     await GloSDK(authToken).boards.cards.comments.create(boardID, cardID, {
       text: 'hello from GH actions'
