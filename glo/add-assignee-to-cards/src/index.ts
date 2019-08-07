cd707a070dee6f27131b8f2f2bb93cbfee059e8f
Dan Suceava <dans@axosoft.com>
2019-08-07 10:59:26 -0700

    add new actions

---
+++ glo/add-assignee-to-cards/src/index.ts
@@ -0,0 +1,65 @@
+import * as core from '@actions/core';
+import GloSDK from '@axosoft/glo-sdk';
+
+interface ICard {
+  boardId: string;
+  cardId: string;
+}
+
+async function run() {
+  const authToken = core.getInput('authToken');
+  const cardsJson = core.getInput('cards');
+  const username = core.getInput('assignee');
+
+  try {
+    const cards = JSON.parse(cardsJson);
+    if (!cards) {
+      return;
+    }
+
+    for (let i=0; i < cards.length; i++) {
+      const cardData = cards[i] as ICard;
+      const { boardId, cardId } = cardData;
+
+      // find the board { id, labels }
+      const board = await GloSDK(authToken).boards.get(boardId, { fields: ['members'] });
+      if (!board) {
+        core.setFailed(`Board ${boardId} not found`);
+        return;
+      }
+      core.debug(JSON.stringify(board));
+
+      // find the card { id, labels }
+      const card = await GloSDK(authToken).boards.cards.get(boardId, cardId, { fields: ['assignees'] });
+      if (!card) {
+        core.setFailed(`Card ${cardId} not found`);
+        return;
+      }
+      core.debug(JSON.stringify(card));
+
+      // find member
+      if (board.members) {
+        const member = board.members.find(m => m.username === username);
+        if (member) {
+          core.debug(JSON.stringify(member));
+
+          if (!card.assignees) {
+            card.assignees = [];
+          }
+
+          // add assignee to the card
+          card.assignees.push({
+            id: member.id as string
+          });
+
+          // update card
+          await GloSDK(authToken).boards.cards.edit(boardId, cardId, card);
+        }
+      }
+    }
+  } catch (error) {
+    core.setFailed(error.message);
+  }
+}
+
+run();
---
+++ glo/add-comment-to-cards/src/index.ts
@@ -0,0 +1,34 @@
+import * as core from '@actions/core';
+import GloSDK from '@axosoft/glo-sdk';
+
+interface ICard {
+  boardId: string;
+  cardId: string;
+}
+
+async function run() {
+  const authToken = core.getInput('authToken');
+  const cardsJson = core.getInput('cards');
+  const comment = core.getInput('comment');
+
+  try {
+    const cards = JSON.parse(cardsJson);
+    if (!cards) {
+      return;
+    }
+
+    for (let i=0; i < cards.length; i++) {
+      const cardData = cards[i] as ICard;
+      const { boardId, cardId } = cardData;
+
+      // add the comment
+      await GloSDK(authToken).boards.cards.comments.create(boardId, cardId, {
+        text: comment
+      });
+    }
+  } catch (error) {
+    core.setFailed(error.message);
+  }
+}
+
+run();
